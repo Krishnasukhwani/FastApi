96b553b98957a301d62c433196563b8eb9212abd
Krishna Sukhwani <krishnasu@cybage.com>
2024-08-12 17:34:35 +0530

    Added schema and few end points

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,6 +1,10 @@
 from pydantic import BaseModel
 
-
+# response_model = response schema
 class Blog(BaseModel):
     title:str
     body: str
+
+class ShowBlog(Blog):
+    class Config():
+        orm_mode = True
